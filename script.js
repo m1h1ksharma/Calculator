c4ac091ed9993057a4317ca1805914b7909ebe19
mahak sharma <123568481+m1h1ksharma@users.noreply.github.com>
2025-07-14 22:49:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,4 +98,34 @@ function clearHistory() {
 }
 
 window.onload = loadHistoryFromLocalStorage;
+document.getElementById('standardModeBtn').addEventListener('click', () => {
+  document.getElementById('standardCalculator').style.display = 'block';
+  document.getElementById('bmiCalculator').style.display = 'none';
+});
 
+document.getElementById('bmiModeBtn').addEventListener('click', () => {
+  document.getElementById('standardCalculator').style.display = 'none';
+  document.getElementById('bmiCalculator').style.display = 'block';
+});
+
+
+function calculateBMI() {
+  const height = parseFloat(document.getElementById("height").value);
+  const weight = parseFloat(document.getElementById("weight").value);
+  const resultDiv = document.getElementById("bmiResult");
+
+  if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+    resultDiv.innerText = "Please enter valid height and weight!";
+    return;
+  }
+
+  const bmi = weight / ((height / 100) ** 2);
+  let category = "";
+
+  if (bmi < 18.5) category = "Underweight";
+  else if (bmi < 24.9) category = "Normal";
+  else if (bmi < 29.9) category = "Overweight";
+  else category = "Obese";
+
+  resultDiv.innerHTML = `Your BMI is <strong>${bmi.toFixed(2)}</strong> (${category})`;
+}
